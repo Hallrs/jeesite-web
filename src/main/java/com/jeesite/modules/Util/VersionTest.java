ff94bc59916e19679744b2c7f863eae4891781fb
Hallrs <18845180198@163.com>
2021-07-13 11:11:36 +0800

    push-2

--- src/main/java/com/jeesite/modules/Util/VersionTest.java
+++ src/main/java/com/jeesite/modules/Util/VersionTest.java
@@ -5,7 +5,7 @@ public class VersionTest {
     public static void main(String[] args) {
 
         System.out.println("qqqq6");
-        System.out.println("qqqq5");
+        System.out.println("qqqq-5");
         System.out.println("qqqq5");
     }
 }
